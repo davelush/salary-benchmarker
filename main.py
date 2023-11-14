c6a862f3dae2e0ce4caf12371c36bf8c5340f32c
davelush <davelush@github.com>
2023-11-14 17:41:32 +0000

    Removing pound signs and percentage signs

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
             try:
                 summary_table = soup.find("table", class_='summary')
                 median_salary = summary_table.find(string=re.compile("Median annual salary")).parent.find_next("td").string.replace(",", "")
+                median_salary = median_salary.replace("£", "")
                 sample_size = summary_table.find(string=re.compile("Number of salaries quoted")).parent.find_next("td").string.replace(",", "")
 
                 # Get the 90th percentile salary (watching for a random `90` in a `fig` class cell)
@@ -35,15 +36,20 @@ if __name__ == '__main__':
                 for candidate in ninetieth_candidates:
                     if not candidate.parent.has_attr("class"):
                         ninetieth_salary = candidate.parent.find_next("td").string.replace(",", "")
+                        ninetieth_salary = ninetieth_salary.replace("£", "")
 
                 # Get the median YoY chance (watching for a missing row because of a new role)
                 median_change_row = summary_table.find(string=re.compile("Median % change year-on-year"))
                 if median_change_row:
-                    median_change = median_change_row.parent.find_next("td").string
+                    median_change_str = median_change_row.parent.find_next("td").string
+                    if median_change_str != "-":
+                        median_change_str = median_change_str.replace("%", "")
+                        median_float = float(median_change_str)/100
+                        median_change_str = f"{median_float:.4f}"
                 else:
-                    median_change = "-"
+                    median_change_str = "-"
 
-                benchmark_row = f"{job.get('Job Title')},{job.get('Reliability')},{job.get('Link')},{job.get('Team')},{median_salary},{ninetieth_salary},{sample_size},{median_change}\n"
+                benchmark_row = f"{job.get('Job Title')},{job.get('Reliability')},{job.get('Link')},{job.get('Team')},{median_salary},{ninetieth_salary},{sample_size},{median_change_str}\n"
                 benchmarks_file.write(benchmark_row)
                 print(benchmark_row)
             except Exception as e:
